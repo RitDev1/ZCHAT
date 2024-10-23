2b2d7f216b06e4220c98d29247452d6bde442b33
RitDev1 <155921700+RitDev1@users.noreply.github.com>
2024-10-23 14:35:33 -0300

    Update ZCHAT.py
    New options, and now the llm is downloaded if the model is not found in the system

--- ZCHAT.py
+++ ZCHAT.py
@@ -16,9 +16,10 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 import textwrap
 import torch
+import subprocess
 import time
-
-
+import sys
+import requests
 def sanitize_filename(filename):
     """Remove caracteres especiais do nome do arquivo."""
     filename = unicodedata.normalize('NFKD', filename).encode('ASCII', 'ignore').decode('ASCII')
@@ -36,9 +37,9 @@ def split_text_into_chunks(text, chunk_size=5000, overlap=100):
 
 from langchain.prompts.chat import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
 
-def generate_summary_llm(text):
+def generate_summary_llm(text, model):
     """Gera um resumo do PDF utilizando a chain `summarize` do LangChain com prompt personalizado."""
-    llm = Ollama(model="llama3.1")  # Define o modelo LLM
+    llm = Ollama(model=model)  # Define o modelo LLM
 
     # Prompt personalizado para a etapa MAP (resumo de trechos)
     map_prompt = ChatPromptTemplate.from_messages([
@@ -102,7 +103,7 @@ def find_abstract_end_page(reader):
             return i + 1
     return None
 
-def create_pdf_with_abstract_or_summary(input_path, output_directory):
+def create_pdf_with_abstract_or_summary(input_path, output_directory, model):
     """Cria um resumo ou usa o abstract se presente no PDF."""
     with open(input_path, 'rb') as f:
         reader = PdfReader(f)
@@ -125,7 +126,7 @@ def create_pdf_with_abstract_or_summary(input_path, output_directory):
         else:
             st.text(f"Abstract não encontrado, gerando resumo para: {input_path}")
             pdf_text = extract_text_from_pdf(reader)
-            summary = generate_summary_llm(pdf_text)
+            summary = generate_summary_llm(pdf_text, model)
 
             temp_pdf_path = os.path.join(output_directory, "temp_summary.pdf")
             create_pdf_with_summary(summary, temp_pdf_path)
@@ -140,12 +141,13 @@ def create_pdf_with_abstract_or_summary(input_path, output_directory):
 
         st.text(f"Novo PDF criado: {output_path}")
 
-def process_pdfs(input_directory, output_directory):
-    """Processa todos os PDFs no diretório de entrada."""
+def process_pdfs(input_directory, output_directory, model):
+    if check_model_exists(model_name) == False:
+        download_model(model_name)
     for filename in os.listdir(input_directory):
         if filename.endswith('.pdf'):
             input_path = os.path.join(input_directory, filename)
-            create_pdf_with_abstract_or_summary(input_path, output_directory)
+            create_pdf_with_abstract_or_summary(input_path, output_directory, model)
 
 def load_pdfs_from_directory(directory):
     """Carrega e retorna uma lista de documentos do diretório."""
@@ -159,7 +161,25 @@ def load_pdfs_from_directory(directory):
                 documents.append(Document(page_content=text, metadata={"source": filename}))
     return documents
 
+def check_model_exists(model_name: str) -> bool:
+    try:
+        response = requests.get(f"http://localhost:11434/models/{model_name}")
+        return response.status_code == 200
+    except requests.ConnectionError:
+        print("Erro: Não foi possível conectar ao serviço Ollama.")
+        return False
+
+def download_model(model_name: str):
+    try:
+        st.text(f"Iniciando o download do modelo: {model_name}...")
+        subprocess.run(["ollama", "pull", model_name], check=True)
+        st.text(f"Download do modelo {model_name} concluído.")
+    except subprocess.CalledProcessError as e:
+        st.text(f"Erro ao baixar o modelo {model_name}: {e}")
+
 def chat_with_model(prompt, model_name, docs, parameter, chunk_size,chunk_overlap):
+    if check_model_exists(model_name) == False:
+        download_model(model_name)
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-mpnet-base-v2")
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     split_documents = text_splitter.split_documents(docs)
@@ -168,7 +188,6 @@ def chat_with_model(prompt, model_name, docs, parameter, chunk_size,chunk_overla
 
     retrieved_docs = retriever.get_relevant_documents(prompt)
     context = "\n\n".join([f"Documento: {doc.metadata['source']}\n{doc.page_content}" for doc in retrieved_docs])
-
     prompt_template = ChatPromptTemplate.from_template(
         "Você está auxiliando uma pesquisa. Responda com base nos documentos fornecidos.\n\n{context}\n\nPergunta: {input}"
     )
@@ -181,17 +200,20 @@ st.title("ZCHAT")
 
 input_directory = st.text_input("Diretório de entrada")
 output_directory = st.text_input("Diretório de saída")
+model_name = st.selectbox("Escolha o modelo de IA", ["llama3.1", "phi 3", "mistral"])
 
 if st.button("Resumir PDFs"):
+    
     if input_directory and output_directory:
-        process_pdfs(input_directory, output_directory)
+        process_pdfs(input_directory, output_directory, model_name)
     else:
         st.warning("Por favor, informe os diretórios.")
 
-parameters = st.slider("Documentos recuperados", min_value=1, max_value=100)
-model_name = st.selectbox("Escolha o modelo de IA", ["llama3.1", "phi 3", "mistral"])
-chunk_size = st.slider("Chunk size", min_value=512, max_value=4096)
-chunk_overlap = st.slider("Chunk overlap", min_value=50, max_value=400)
+parameters = st.slider("Documentos recuperados", min_value=1, max_value=100, value=10)
+mostrar_avançadas = st.checkbox("Mostrar configurações avançadas")
+if mostrar_avançadas: 
+    chunk_size = st.slider("Chunk size", min_value=512, max_value=4096, value=1024)
+    chunk_overlap = st.slider("Chunk overlap", min_value=50, max_value=400,value=100)
 prompt = st.text_area("Digite seu prompt")
 
 if st.button("Rodar IA"):
